cc0cc8af0f855ea63abdbb113a499c49703e963f
Cedric-dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-10-25 13:13:04 -0400

    changes 1

--- main.ts
+++ main.ts
@@ -1,23 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Cedric
+ * Created on: Oct 2023
  * This program measures the distance between objects
 */
 
-let distanceToObject:number = 0
+let distanceToObject: number = 0
 
 // setup
 basic.showIcon(IconNames.Happy)
 
 // find distance from sonar
-input.onButtonPressed(Button.A, function() {
-    basic.clearScreen()
-    distanceToObject = sonar.ping(
-        DigitalPin.P1,
-        DigitalPin.P2,
-        PingUnit.Centimeters
-    )
-    basic.showNumber(distanceToObject)
-    basic.showIcon(IconNames.Happy)
+input.onButtonPressed(Button.A, function () {
+  basic.clearScreen()
+  distanceToObject = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+  )
+  basic.showNumber(distanceToObject)
+  basic.showIcon(IconNames.Happy)
 })
